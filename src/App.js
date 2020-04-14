fbbdf46f7cbe5f57a20965f64d2348643ca84b6e
NateTheDev1 <nthnlrichards@gmail.com>
2020-04-13 22:26:07 -0400

    Data Fetch Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const API_KEY = "NVYidd3P4hx1R46ts9fBdswKkbllBG7m9tu5PEmB";
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <p>
